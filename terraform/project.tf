503b9b9559568dacbaf685d02a055d7019000252
dsmoliansky <dsmoliansky@yoobic.com>
2020-04-05 17:47:23 +0100

    create secrets template

--- terraform/project.tf
+++
@@ -1,9 +0,0 @@
-resource "google_project" "my_project" {
-  name       = "My Project"
-  project_id = "your-project-id"
-  org_id     = "1234567"
-}
-
-resource "random_id" "project_name_suffix" {
-  byte_length = 4
-}
